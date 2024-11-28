915508b75a23d031c8737873f506b822cc0a4553
Muharrem ERIN <erin.muharrem@gmail.com>
2024-11-28 10:20:25 +0300

    #refactoring

--- src/main/kotlin/com/mewebstudio/blogapi/util/extensions.kt
+++ src/main/kotlin/com/mewebstudio/blogapi/util/extensions.kt
@@ -4,5 +4,5 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 fun <R : Any> R.logger(): Lazy<Logger> = lazy {
-    LoggerFactory.getLogger((if (javaClass.kotlin.isCompanion) javaClass.enclosingClass else javaClass).name)
+    LoggerFactory.getLogger(if (this::class.isCompanion) this::class.java.enclosingClass else this::class.java)
 }
